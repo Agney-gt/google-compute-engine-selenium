f5a4acae654ed0725daa5d575b08703e67e34eba
Agney Nalapat <84612798+Agney-gt@users.noreply.github.com>
2023-10-16 11:04:58 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,15 +8,6 @@ display.start()
 
 url = 'http://www.python.org'
 
-print('browsing with firefox, ', url)
-try:
-  browser = webdriver.Firefox()
-  browser.get(url)
-  print(browser.title)
-  browser.quit()
-except Exception as e:
-  print e
-
 print('browsing with chrome, ', url)
 try:
   browser = webdriver.Chrome()
